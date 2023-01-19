fd09f835abf5e70b62d650b3d32101e05191be23
mohamedsam902 <mohamedsam9029@gmail.com>
2023-01-19 14:48:43 +0200

    Some Update Serve

---
+++ storage/framework/views/3a86528f454b58c0b1af08e143597cef3f5a46bd.php
@@ -0,0 +1,48 @@
+<?php $__env->startSection('content'); ?>
+    <!--Page Header Start-->
+    <section class="page-header">
+        <div class="page-header-bg" style="background-image: url(<?php echo e(asset('site')); ?>/assets/images/resources/WhatsApp\ Image.png)">
+        </div>
+        <div class="container">
+            <div class="page-header__inner">
+
+            </div>
+        </div>
+    </section>
+    <!--Page Header End-->
+
+    <!--About Four Start-->
+    <section class="about-four " style="padding:  100px 0;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-7 " style=" display: flex;
+                align-items: center; ">
+                    <div class="about-four__right">
+                        <div class="section-title text-left">
+                            <h2 class="section-title__title"><?php echo e($impact->title); ?></h2>
+                        </div>
+                        <p class="about-four__text"><?php echo e($impact->content); ?></p>
+                        <div class="">
+                            <a href="<?php echo e(route('healthcare')); ?>" class=" main-slider__btn Healthcare">  <?php echo e(__('site.Health')); ?> </a>
+                            <a href="<?php echo e(route('wellbeing')); ?>" class=" main-slider__btn being"> <?php echo e(__('site.Well')); ?></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5">
+                    <div class="about-four__left " style="margin-top: 70px;">
+                        <div class="about-four__img-box impact_img">
+                            <div class="about-four__img-box impact_img" style="left: 15px;">
+                                <img src="<?php echo e($impact->getFirstMediaUrl('Impact')); ?>" class="w-100" alt=" ">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--About Four End-->
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('site.layout.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/perfectchoice/public_html/kater/resources/views/site/impact.blade.php ENDPATH**/ ?>
