d88b468351c6c2d516d81a57e60506d340b79b08
mohamedsamy902 <mohamedsamy9029@gmail.com>
2023-01-22 02:45:50 +0200

    v2

--- storage/framework/views/87b03104b9e2e3a283bf85f7ddbc74304d05a986.php
+++ storage/framework/views/87b03104b9e2e3a283bf85f7ddbc74304d05a986.php
@@ -3,7 +3,7 @@
     <section class="gallery-page">
         <h1 class="text-center pb-3"><?php echo e($gall->title); ?></h1>
         <div class="container">
-        <p class="pb-5 text-center"><?php echo e($gall->content); ?></p>
+        <p class="pb-5 text-center"><?php echo $gall->content; ?></p>
 
 
             <div class="row">
@@ -12,7 +12,7 @@
                         <div class="gallery-page__single">
                             <div class="gallery-page__img">
                                 <img src="<?php echo e($gallery->getFirstMediaUrl('galleryDetails')); ?>">
-                                <a href="" class="img-popup"></a>
+                                <a href="<?php echo e($gallery->getFirstMediaUrl('galleryDetails')); ?>" class="img-popup"></a>
                             </div>
                         </div>
                     </div>
